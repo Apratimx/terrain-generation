4409c149d2140cab72a7161ad6c465186cb0a41a
AseCoder <51834762+AseCoder@users.noreply.github.com>
2019-07-07 20:59:43 +0300

    update

--- main.js
+++ main.js
@@ -1,52 +1,24 @@
 function setup() {
-  console.log('---- NEW TERRAIN ---- ---- NEW TERRAIN ---- ---- NEW TERRAIN ----');
-  createCanvas(800, 500);
-  background(133, 197, 255);
+  createCanvas(1920, 750);
   strokeCap(SQUARE);
-
-  // draw dirt and grass
-    // user vars
-
-  var hillSeverity = parseInt(document.getElementById('hillSeverity').value); //how strong hills
-  var terrainRes = parseInt(document.getElementById('terrainRes').value); //how small pixels
-  var hillRapidness = parseInt(document.getElementById('hillRapidness').value); //how often to change drawing direction
-  var terrainBumpiness = parseInt(document.getElementById('terrainBumpiness').value); //how much to vary from the normal drawing direction
-  console.log(`${hillSeverity}, ${terrainRes}, ${hillRapidness}, ${terrainBumpiness}`);
-
-  var fooGen = 'Math.floor(Math.random() * (hillSeverity * 2 + 1)) - hillSeverity';
-  var foo = eval(fooGen) * 2;
-  var point = foo;
-  var tops = [];
-  strokeWeight(terrainRes);
-  for (var i = 0; i < width + terrainRes; i += terrainRes) {
-    if (i % (terrainRes * hillRapidness) === 0) {
-      foo = eval(fooGen);
-      console.log('hillSeverity Changed');
+  console.log('------------------- NEW TERRAIN -------------------');
+  var hillSeverity = parseFloat(document.getElementById('hillSeverity').value);
+  var terrainRes = parseFloat(document.getElementById('terrainRes').value);
+  var climbingDirectionChange = parseFloat(document.getElementById('climbingDirectionChange').value);
+  var hillPoints = [Math.random() * 2 - 1];
+  var climbingDirection = (hillSeverity / 2) - (Math.random() * hillSeverity);
+  console.log(climbingDirection);
+  if (terrainRes < 1) {
+    terrainRes = 1;
+  }
+  for (var i = 1; i < width / terrainRes; i++) {
+    console.log(hillPoints[i - 1]);
+    hillPoints[i] = hillPoints[i - 1] + climbingDirection;
+    if (i % climbingDirectionChange === 0) {
+      climbingDirection = (climbingDirection * 0.3) + (hillSeverity / 2) - (Math.random() * hillSeverity);
+      console.log(`climbingDirection: ${climbingDirection}`);
     }
-    point += foo;
-    var drawPoint = (height * 0.7) + point + (Math.random() - 0.5) * terrainBumpiness;
-    console.log(drawPoint);
-    stroke(79, 60, 39);
-    line(i, 600, i, drawPoint);
-    stroke(29, 173, 0);
-    line(i, drawPoint - 10, i, drawPoint + 2);
-    tops[tops.length] = Math.ceil(drawPoint - 10);
+    strokeWeight(terrainRes + 1);
+    line(i * terrainRes, height, i * terrainRes, hillPoints[i] + height / 2);
   }
-  console.log(tops.length);
-  var trees = Math.floor(3 + Math.random() * 9);
-  for (var i = 1; i < trees + 1; i++) {
-    tree(((width / trees) * i), tops[Math.floor((tops.length / trees) * i)] + 5);
-  }
-  console.log('trees done');
-
-}
-function tree(x, y) {
-  stroke(74, 70, 56);
-  strokeWeight(12);
-  var trunkTop = 25 + Math.floor(Math.random() * 20);
-  line(x, y, x, y - trunkTop);
-  stroke(35, 204, 66);
-  fill(35, 204, 66);
-  circle(x, y - trunkTop * 1.5, trunkTop);
-
 }
